c429f07d46ed29dad78299c4a1d789f734a85af0
IsaoTakahashi <ricto415@gmail.com>
2012-10-27 08:02:39 +0900

    for study session

---
+++ ThirtyMinutes/BaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableViewController.h
+//  ThirtyMinutes
+//
+//  Created by 高橋 勲 on 2012/10/26.
+//  Copyright (c) 2012年 高橋 勲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTableViewController : UITableViewController
+
+@end
---
+++ ThirtyMinutes/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  ThirtyMinutes
+//
+//  Created by 高橋 勲 on 2012/10/26.
+//  Copyright (c) 2012年 高橋 勲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
